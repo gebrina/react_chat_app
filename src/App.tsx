6cc612e35ac121e72a0754dd57b281124687e8aa
gebrina <gsonofmika@gmail.com>
2023-11-18 21:35:15 +0300

    create project and install dependencies

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Login from "./pages/Login";
+
+export default function App() {
+  return (
+    <main className="main-container">
+      <section className="container py-5 mx-auto">
+        <Login />
+      </section>
+    </main>
+  );
+}
