a2b9778ff80e996f9fa7857fd2794e1cce8f5a86
gebrina <gsonofmika@gmail.com>
2023-11-19 19:21:12 +0300

    added delete, edit, nextpage, prevpage icons

--- src/components/users/User.tsx
+++ src/components/users/User.tsx
@@ -1,12 +1,26 @@
 import { FC } from "react";
 import { TUser } from "../../types/User";
+import { FaEdit, FaTrash } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
 type UserProps = {
   user: TUser;
 };
 
 const User: FC<UserProps> = ({ user: { username } }) => {
-  return <div>{username}</div>;
+  return (
+    <div className="px-3 items-center flex justify-between  border-[1px] w-full  sm:w-1/3 text-center capitalize border-opacity-20 rounded hover:border-opacity-50  border-green-950 ">
+      <FaEdit className="cursor-pointer hover:text-opacity-60 text-green-600" />
+
+      <NavLink
+        className="flex-1 py-2 cursor-pointer hover:animate-pulse font"
+        to={`/${username}/chat`}
+      >
+        <span>{username}</span>
+      </NavLink>
+      <FaTrash className="cursor-pointer hover:text-opacity-60 text-red-600" />
+    </div>
+  );
 };
 
 export default User;
--- src/components/users/index.tsx
+++ src/components/users/index.tsx
@@ -3,6 +3,7 @@ import { getUsers } from "../../api";
 import UserForm from "./Form";
 import { TUser } from "../../types/User";
 import User from "./User";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const Users = () => {
   const [users, setUsers] = useState<TUser[]>([]);
@@ -18,9 +19,15 @@ const Users = () => {
   return (
     <section>
       <UserForm users={users} setUsers={setUsers} />
-      {users.map((user) => (
-        <User user={user} key={user.id} />
-      ))}
+      <section className="flex my-5 items-center flex-col gap-2">
+        {users.map((user) => (
+          <User user={user} key={user.id} />
+        ))}
+        <div className="flex gap-10 my-2">
+          <FaArrowLeft className="text-green-900 text-xl hover:text-opacity-50 cursor-pointer" />
+          <FaArrowRight className="text-green-900 text-xl hover:text-opacity-50 cursor-pointer" />
+        </div>
+      </section>
     </section>
   );
 };
