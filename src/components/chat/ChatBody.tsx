c5e4fbf5140b200e1804c03883d1d95ff28618ad
gebrina <gsonofmika@gmail.com>
2023-11-25 15:06:42 +0300

    chat : add style for chat box and different users

--- src/components/chat/ChatBody.tsx
+++ src/components/chat/ChatBody.tsx
@@ -1,14 +1,30 @@
 import { FC } from "react";
 import { TChat } from "../../types/Chat";
+import { useChatContext } from "../../context/UseChatContext";
 
 type ChatBodyProps = {
   id: string;
   chat: TChat;
 };
 
-const ChatBody: FC<ChatBodyProps> = ({ chat, id }) => {
-  const { message } = chat;
-  return <div className="bg-slate-600 rounded-lg ">{message}</div>;
+const ChatBody: FC<ChatBodyProps> = ({ chat }) => {
+  const { currentUser } = useChatContext();
+
+  const {
+    message,
+    user: { username },
+  } = chat;
+  return (
+    <p
+      className={`${
+        username === currentUser?.username
+          ? "bg-slate-300  translate-x-[95%]"
+          : "bg-blue-300"
+      } w-1/2 mx-2 p-2 my-1 text-xl rounded-lg`}
+    >
+      {message}
+    </p>
+  );
 };
 
 export default ChatBody;
